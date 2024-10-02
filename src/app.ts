77e4dd96b38ed2c278011851e32a2fa18f8cffa9
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2024-10-02 22:24:15 +0200

    Basic interface
    This will be the last change before the first creation of branches.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 class App{
-    
+    addBlockedPage(url: string){
+        chrome.runtime.sendMessage({action: "addBlockedPage", url: url});
+    }
 }
 new App();
---
+++ src/popup.ts
@@ -0,0 +1,62 @@
+// This js file will be used for displaying time information etc...
+class Popup {
+    htmlDoc : Document;
+    selectElement : HTMLElement | null;
+    disabledSelect : HTMLElement | null;
+    constructor() {
+        this.htmlDoc = document;
+        this.selectElement = this.htmlDoc.getElementById("theme-select");
+        this.disabledSelect = this.htmlDoc.getElementById("disabled-select");
+        this.loadTheme();
+        this.selectElement?.addEventListener("change", (event) => {
+            this.changeTheme((event.target as HTMLSelectElement).value);
+        });
+    }
+    loadTheme() {
+        let theme : string | null = "";
+        if(localStorage.getItem("Theme") != null) {
+            theme = (localStorage.getItem("Theme"));
+            console.log(theme);
+        }
+        const existingLink = this.htmlDoc.getElementById("theme-stylesheet");
+        if (existingLink) {
+            existingLink.remove();
+        }
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.id = 'theme-stylesheet';
+        if(theme == "dark") {
+            link.href = '/popup/dark.css';
+        }
+        else if(theme == "light") {
+            link.href = '/popup/light.css';
+        }
+        else if(theme == "solarized") {
+            link.href = '/popup/solarized.css';
+        }
+        else {
+            link.href = '/popup/brown.css';
+        }
+        if(this.disabledSelect != null && theme) {
+            if(theme == "light") {
+                this.disabledSelect.innerText = "Light Theme";
+            }
+            else if(theme == "dark") {
+                this.disabledSelect.innerText = "Dark Theme";
+            }
+            else if(theme == "solarized") {
+                this.disabledSelect.innerText = "Solarized Theme";
+            }
+            else {
+                this.disabledSelect.innerText = "Brown Theme";
+            }
+        }
+        this.htmlDoc.head.appendChild(link);
+    }
+    changeTheme(theme : string) {
+        localStorage.setItem("Theme", theme);
+        this.loadTheme();
+    }
+}
+new Popup();
