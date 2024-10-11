879468e0a152fbccd8a4ac70bfc95c86c5ba8c20
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2024-10-11 14:07:09 +0200

    Content-script for app
    Works in every individual tab independently, however import statement needs help with being a non - module.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
+import * as Site from './sites.js';
+import { Sites } from './types';
+// import { Sites, SiteElement as SiteType } from './types';
 class App{
-    addBlockedPage(url: string){
-        chrome.runtime.sendMessage({action: "addBlockedPage", url: url});
+    url : string;
+    Site : any
+    siteKey : string
+    constructor(){
+        this.url = window.location.href;
+        this.siteKey = "";
+        this.Site = Site
+        console.log("My ts file is loaded!")
+        this.Site.retrieveSites((siteResponse : Sites) => {
+            for(const currSite in siteResponse){
+                console.log(currSite);
+            }
+        })
+    }
+    checkTimeout(){
+        // This function returns whether or not there is a timeout on the page and return the amount (0 if none)
+        
     }
 }
 new App();
